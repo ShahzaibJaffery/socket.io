ebe0ca4f73cbbc6847e108474f8204fa53436e84
unknown <shahzaibjafferi888@gmail.com>
2018-11-15 02:53:27 +0500

    Changes update 1

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ const app = express();
 
 app.use(express.static('public'));
 
+  app.get('*',(req,res)=>{
+        res.sendFile(__dirname + '/public/index.html');
+});
 
-const server = app.listen(3000,()=>{
-    console.log('Listen to port 3000');
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //Socket Setup
