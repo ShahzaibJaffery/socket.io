bc13a43fc8dda8d7713d85138ee5274dbdf2cb18
ShahzaibJaffery <shahzaibjafferi888@gmail.com>
2019-10-29 13:56:47 +0500

    New Changes 29

--- public/chat.js
+++ public/chat.js
@@ -18,21 +18,46 @@ const name = 'user'+Math.floor((Math.random() * 1000) + 3);
 
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
+    var name1 = $('#handle').val();
+    var message1 = $('#message').val();
+
+    if($.trim(name1).length == 0 && $.trim(message1).length == 0){
+        alert("Please enter your name and message");
+    }else if($.trim(name1).length == 0){
+        alert("Please enter your name");
+    }else if($.trim(message1).length == 0){
+    alert("Please enter your message");
+    }else{
+    
     socket.emit('chat',{
         message : message.value,
         handle : handle.value
     });
+}
 });
 
 
 message.addEventListener('keypress',(e)=>{
        socket.emit('typing',{message : "user is typing..."});
         if(e.keyCode == 13){
+        $("#chat-window").stop().animate({ scrollTop: $("#mario-chat")[0].scrollHeight}, 1000);
+        var name1 = $('#handle').val();
+        var message1 = $('#message').val();
+
+        if($.trim(name1).length == 0 && $.trim(message1).length == 0){
+            alert("Please enter your name and message");
+        }else if($.trim(name1).length == 0){
+            alert("Please enter your name");
+        }else if($.trim(message1).length == 0){
+            alert("Please enter your message");
+        }else{
          socket.emit('chat',{
             message : message.value,
             handle : handle.value
 
         });
+
+    }
    }
 });
 
@@ -45,6 +70,8 @@ message.addEventListener("keyup", () =>  {
 //Load Old Messages
 socket.on('loadold',(data)=>{
     output.innerHTML = "";
+    $("#chat-window").stop().animate({ scrollTop: $("#mario-chat")[0].scrollHeight}, 1000);
+
     for (var i=0; i < data.length; i++) {
      output.innerHTML += '<P><strong>'+data[i].name+': </strong>'+data[i].msg+'</P>';
     }
@@ -54,6 +81,7 @@ socket.on('loadold',(data)=>{
 
 socket.on('chat',(data)=>{
     feedback.innerHTML = "";
+    $("#chat-window").stop().animate({ scrollTop: $("#mario-chat")[0].scrollHeight}, 1000);
     output.innerHTML += '<P><strong>'+data.handle+': </strong>'+data.message+'</P>';
     message.value = "";
 });
